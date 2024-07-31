0aa22a6d9242b8af24a309d493b005aa4a5a34f1
Arnold Angelo <aangeloarnold@gmail.com>
2024-07-31 16:40:51 +1000

    Initial API

--- routes.js
+++ routes.js
@@ -4,9 +4,11 @@ var app = express();
 // Defining all the routes
 var index = require('./routes/index');
 var users = require('./routes/users');
+var allocation = require('./routes/allocation');
 
 // Linking all the routes
 app.use('/', index);
 app.use('/users', users);
+app.use('/allocation', allocation)
 
 module.exports = app;
